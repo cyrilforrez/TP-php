a474c6ace33d1aa4350b90cf6875c9181317c76b
cyrilforrez <cyilforrez@gmail.com>
2017-09-07 13:46:04 +0200

    create html

---
+++ secret.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Codes d'accès au serveur central de la NASA</title>
+</head>
+
+<body>
+    <h1>Voici les codes d'accès :</h1>
+    <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>
+</body>
+
+</html>
