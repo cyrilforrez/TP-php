35e3047d2f85b32e27c475fecc123be3b0807f0a
cyrilforrez <cyilforrez@gmail.com>
2017-09-08 09:51:44 +0200

    add if

--- secret.php
+++ secret.php
@@ -3,12 +3,17 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Codes d'accès au serveur central de la NASA</title>
+    <title>Codes d'accès au serveur central</title>
 </head>
 
 <body>
+<?php
+
+    if (isset($_POST['mdp']) AND $_POST['mdp'] ==  "kangourou") {
+?>
     <h1>Voici les codes d'accès :</h1>
     <p><strong>CRD5-GTFT-CK65-JOPM-V29N-24G1-HH28-LLFV</strong></p>
+
 </body>
 
 </html>
