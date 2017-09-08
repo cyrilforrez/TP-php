86417878128461a575f6b83e4185c4d25d9f567d
cyrilforrez <cyilforrez@gmail.com>
2017-09-08 09:51:25 +0200

    modif html

--- formulaire.php
+++ formulaire.php
@@ -7,14 +7,13 @@
 </head>
 
 <body>
-    <p>Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central de la NASA :</p>
+    <p>Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central :</p>
     <form action="secret.php" method="post">
         <p>
             <input type="password" name="mdp" />
             <input type="submit" value="Valider" />
         </p>
     </form>
-    <p>Cette page est réservée au personnel de la NASA. Si vous ne travaillez pas à la NASA, inutile d'insister vous ne trouverez jamais le mot de passe ! ;-)</p>
 </body>
 
 </html>
